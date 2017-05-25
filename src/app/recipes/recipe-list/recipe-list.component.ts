b328d1279bf165399552bedeed10b47474d82c02
rhlebion <rafal.hlebionek@capgemini.com>
2017-05-25 12:41:14 +0200

    components excercises

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Hotdog', 
+               'A recipe for the most beutiful hotdog You\'ve ever eaten',
+               'http://8exll2rwl8l1f2ayejq2l9yh.wpengine.netdna-cdn.com/wp-content/uploads/2014/10/hotdog_mustard-main.jpg')  
+  ];
 
   constructor() { }
 
